5d3a7f39fe259abc62c9837c12c879ddb777c0bd
Mreow8 <honzelchavez100@gmail.com>
2025-03-15 18:17:47 +0800

    ERtr

--- KwentasKlaras/settings.py
+++ KwentasKlaras/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+PORT = os.getenv("PORT", "10000")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SESSION_ENGINE = "django.contrib.sessions.backends.db"  # Store sessions in the DB
